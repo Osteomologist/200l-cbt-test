deaf258d924f3822468845888b03801ade5df21c
Osteomologist <ochiemebhoyaemmanuel@gmail.com>
2026-05-15 03:59:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const questions = [
 
+{
+question: "Which nerve supplies the deltoid muscle?",
+options: ["Radial", "Median", "Axillary", "Ulnar"],
+answer: 2
+},
+
+{
+question: "The femur is found in the upper limb.",
+options: ["True", "False"],
+answer: 1
+}
+
+];
+
+let currentQuestion = 0;
+
+const questionContainer = document.getElementById("question-container");
+
+function loadQuestion() {
+
+const q = questions[currentQuestion];
+
+questionContainer.innerHTML = `
+<h2>${q.question}</h2>
+
+${q.options.map(option =>
+`
+<button>${option}</button>
+`
+).join("")}
+`;
+
+}
+
+loadQuestion();
