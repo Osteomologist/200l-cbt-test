3bbcc778ce67c72f7f933ab14657d86f20c46b20
Osteomologist <ochiemebhoyaemmanuel@gmail.com>
2026-05-15 04:12:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,23 +15,96 @@ answer: 1
 ];
 
 let currentQuestion = 0;
+let answers = [];
 
 const questionContainer = document.getElementById("question-container");
+const questionNumber = document.getElementById("question-number");
+const questionGrid = document.getElementById("question-grid");
 
-function loadQuestion() {
+function loadQuestion(){
 
 const q = questions[currentQuestion];
 
+questionNumber.innerHTML =
+`Question ${currentQuestion + 1} of ${questions.length}`;
+
 questionContainer.innerHTML = `
+
 <h2>${q.question}</h2>
 
-${q.options.map(option =>
+${q.options.map((option,index)=>
+
 `
-<button>${option}</button>
+<button class="option-btn"
+onclick="selectAnswer(${index})">
+
+${option}
+
+</button>
 `
+
 ).join("")}
 `;
 
 }
 
+function selectAnswer(index){
+
+answers[currentQuestion] = index;
+
+document.querySelectorAll(".q-box")[currentQuestion]
+.style.background = "green";
+
+}
+
+document.getElementById("next-btn")
+.addEventListener("click",()=>{
+
+if(currentQuestion < questions.length - 1){
+
+currentQuestion++;
+loadQuestion();
+
+}
+
+});
+
+document.getElementById("prev-btn")
+.addEventListener("click",()=>{
+
+if(currentQuestion > 0){
+
+currentQuestion--;
+loadQuestion();
+
+}
+
+});
+
+function loadPalette(){
+
+questions.forEach((q,index)=>{
+
+questionGrid.innerHTML += `
+
+<div class="q-box"
+onclick="jumpQuestion(${index})">
+
+${index + 1}
+
+</div>
+`;
+
+});
+
+}
+
+function jumpQuestion(index){
+
+currentQuestion = index;
+loadQuestion();
+
+}
+
+loadPalette();
 loadQuestion();
